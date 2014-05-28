66fe1a2f703fc103bcf74e0cc9a569b4d05d1ca1
arichter <arichter@navngo.com>
2014-05-28 14:51:24 +0200

    a

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -13,12 +13,45 @@ import java.util.List;
 public class Player
 {
     static final String VERSION = "Royal Pityoka AI player";
+    private final static String PLAYER_SHORTY = "Get Shorty";
     private static Util util = new Util();
     private static Batman batman = new Batman();
 
     public static boolean isHigh(String figure)
     {
         return figure.equals("A") || figure.equals("J") || figure.equals("K") || figure.equals("Q") || figure.equals("10");
+    }
+
+    public static boolean isPlayerAllIn(GameSpace gameSpace, String strPlayer)
+    {
+        boolean result = false;
+        PlayerObj player = getPlayerByName(gameSpace, strPlayer);
+
+        if (player == null)
+        {
+            System.err.println("user cannot be found by name");
+        }
+        else
+        {
+            result = (player.getBet() > 0) && (player.getStack() == 0);
+        }
+
+        return result;
+    }
+
+    public static PlayerObj getPlayerByName(GameSpace gameSpace, String name)
+    {
+        PlayerObj result = null;
+
+        for (PlayerObj player : gameSpace.getPlayers())
+        {
+            if (player.getName().equalsIgnoreCase(name))
+            {
+                return player;
+            }
+        }
+
+        return result;
     }
 
     public static int betRequest(JsonElement request)
@@ -72,16 +105,23 @@ public class Player
             result = gameSpace.getMinimumRaise();
         }
 
+        if (isPlayerAllIn(gameSpace, PLAYER_SHORTY) && (rank > 23))
+        {
+            result = pokerPlayer.getStack();
+        }
+        else
+        {
+            if (isPlayerAllIn(gameSpace, PLAYER_SHORTY))
+            {
+                result = 0;
+            }
+        }
+
         return result;
     }
 
     public static void showdown(JsonElement game)
     {
-    }
-
-    public boolean isShortyAllIn()
-    {
-        return false;
     }
 
     private static int[] toArray(List<Card> community, List<Card> hole)
