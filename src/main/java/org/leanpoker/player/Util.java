8555ce57e54d5b675166a5b4af6bfa4d262c0539
Ferenc Sárai <ferenc.SARAI@nng.com>
2014-05-28 12:29:00 +0200

    compare

--- src/main/java/org/leanpoker/player/Util.java
+++ src/main/java/org/leanpoker/player/Util.java
@@ -1,9 +1,33 @@
 package org.leanpoker.player;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util
 {
+    public Map<Integer, Integer> compare(final Map<Integer, Integer> player, final Map<Integer, Integer> other)
+    {
+        Map<Integer, Integer> retV = new HashMap<Integer, Integer>(player.size());
+
+        for (Integer playerKey : player.keySet())
+        {
+            for (Integer otherKey : other.keySet())
+            {
+                final Integer otherValue = player.get(otherKey);
+
+                if (playerKey > otherKey)
+                {
+                    final Integer current = player.get(playerKey);
+
+                    retV.put(playerKey, current + otherValue);
+                }
+            }
+        }
+
+        return retV;
+    }
+
     public int[] sort(final int[] input)
     {
         Arrays.sort(input);
