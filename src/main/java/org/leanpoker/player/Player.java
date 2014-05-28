2b88e00b936209e9c5a9b8f4f05b6621304affa2
arichter <arichter@navngo.com>
2014-05-28 11:31:56 +0200

    a

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,13 +2,18 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
+import org.leanpoker.player.obj.GameSpace;
+import org.leanpoker.player.parse.Convert;
+
 public class Player
 {
     static final String VERSION = "Royal Pityoka AI player";
 
     public static int betRequest(JsonElement request)
     {
-        return 400;
+        GameSpace gameSpace = Convert.getGameSpace(request);
+
+        return 50;
     }
 
     public static void showdown(JsonElement game)
