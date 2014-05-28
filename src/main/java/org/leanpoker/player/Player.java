e0a365352d9562335741b3dd43edb5cfcaa7aa2e
arichter <arichter@navngo.com>
2014-05-28 10:17:45 +0200

    mindig emelunk

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,16 +2,16 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
-
-public class Player {
-
+public class Player
+{
     static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonElement request) {
-        return 0;
+    public static int betRequest(JsonElement request)
+    {
+        return 50;
     }
 
-    public static void showdown(JsonElement game) {
+    public static void showdown(JsonElement game)
+    {
     }
 }
