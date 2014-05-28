d97a2918c7c5049eea2138b8b0b43e90713198a0
unknown <tnagy@Tnagy.NavNGo.local>
2014-05-28 14:14:30 +0200

    c6

--- src/main/java/org/leanpoker/player/Util.java
+++ src/main/java/org/leanpoker/player/Util.java
@@ -1,9 +1,29 @@
 package org.leanpoker.player;
 
+import org.leanpoker.player.obj.GameSpace;
+import org.leanpoker.player.obj.PlayerObj;
+
 import java.util.Arrays;
 
 public class Util
 {
+    public static PlayerObj getBatman(GameSpace gameSpace)
+    {
+        PlayerObj result = null;
+
+        for (PlayerObj player : gameSpace.getPlayers())
+        {
+            if ((player.getHoleCards() != null) && !player.getHoleCards().isEmpty())
+            {
+                result = player;
+
+                break;
+            }
+        }
+
+        return result;
+    }
+
     public int[] sort(final int[] input)
     {
         Arrays.sort(input);
