ff800ac6859074557eba05d6bae10b7be6f416b5
unknown <tnagy@Tnagy.NavNGo.local>
2014-05-28 12:15:41 +0200

    c4

--- src/test/java/org/leanpoker/player/GsonTest.java
+++ src/test/java/org/leanpoker/player/GsonTest.java
@@ -1,15 +1,27 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.junit.Test;
+
+import org.leanpoker.player.obj.GameSpace;
+import org.leanpoker.player.parse.Convert;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class GsonTest
 {
-    /*
+    private static final String TEST_JSON = "../../../../../resources/test/test.json";
+
     @Test public void testGson() throws Exception
     {
-    final JsonParser parser = new JsonParser();
-    final FileReader fileReader = new FileReader("d:\\temp\\gson1.txt");
-    final JsonElement jsonElement = parser.parse(fileReader);
-    GameSpace gameSpace = Convert.getGameSpace(jsonElement);
+        final JsonParser parser = new JsonParser();
+        final Reader reader = new InputStreamReader(this.getClass().getResourceAsStream(TEST_JSON));
+        final JsonElement jsonElement = parser.parse(reader);
+        GameSpace gameSpace = Convert.getGameSpace(jsonElement);
 
-    System.out.println(gameSpace);
-    }*/
+        System.out.println(gameSpace);
+    }
 }
